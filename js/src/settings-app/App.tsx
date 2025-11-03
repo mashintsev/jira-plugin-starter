b116b615596c7acb59d60eff8533cff91cb30f76
mashintsev <mashintsev@gmail.com>
2025-11-03 23:56:35 +0300

    configure js modules

---
+++ js/src/settings-app/App.tsx
@@ -0,0 +1,20 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createRoot } from "react-dom/client";
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      Hello world, React App!
+    </QueryClientProvider>
+  );
+}
+
+export default function init() {
+  const domNode = document.getElementById("starter-settings-container");
+  if (domNode) {
+    const root = createRoot(domNode);
+    root.render(<App />);
+  }
+}
---
+++ js/src/settings-app/index.ts
@@ -0,0 +1,6 @@
+import AJS from "AJS";
+import init from "./App";
+
+AJS.toInit(() => {
+  init();
+});
